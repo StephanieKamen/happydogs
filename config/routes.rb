b5e809b136b206623f5a6c6f9063559c6cc3fb95
johanna gmelin <johanna.gmelin@gmail.com>
2021-08-03 21:40:25 +0200

    booking-model

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,8 @@
   root to: 'pages#home'
   resources :users, except: [:update, :destroy, :edit]
   resources :doghomes, only: [:index, :show]
+  resources :doghomes do
+    resources :bookings, only: [:create]
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
